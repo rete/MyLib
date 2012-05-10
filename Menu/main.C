a23fb5153fda323b050bf0df874f2309ee15a61a
Eté Rémi <remi.ete@gmail.com>
2012-05-10 17:49:02 +0200

    main added

---
+++ Menu/main.C
@@ -0,0 +1,33 @@
+/*
+ *
+ * //My libraries for C++ usage
+ * Copyright (c) 2012 Rémi Eté
+ * All Right Reserved.
+ *
+ * Use and copying of these libraries and preparation of derivative works
+ * based upon these libraries are permitted. Any copy of these libraries
+ * must include this copyright notice.
+ *
+ * Written by : R. Eté
+ */
+
+#include <iostream>
+#include <string>
+#include "menu.h"
+#include "Option.h"
+#include "OptionLayout.h"
+
+using namespace std ;
+
+int main(int argc, char * argv[])
+{
+
+Menu menu ;
+menu.setExecutable("SoftTest") ;
+menu.setExampleLine("'SoftTest -xyz' will start the program") ;
+menu.setEmailAdress("remi.ete@gmail.com") ;
+menu.setSourceCodeAdress("https://github.com/rete/IsingModel") ;
+menu.Usage() ;
+
+	return 0 ;
+}
