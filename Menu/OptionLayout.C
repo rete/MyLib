279ccc4ae3bfbaba5ddba201cb956c21baa09b8b
Eté Rémi <remi.ete@gmail.com>
2012-05-20 20:49:22 +0200

    OptionLayout added

--- Menu/OptionLayout.C
+++ Menu/OptionLayout.C
@@ -10,3 +10,64 @@
  *
  * Written by : R. Eté
  */
+
+#include "OptionLayout.h"
+
+using namespace std ;
+
+OptionLayout::OptionLayout(string t)
+{
+	title = t ;
+}
+
+OptionLayout::~OptionLayout()
+{
+
+}
+
+int OptionLayout::listSize()
+{
+	return optionList.size() ;
+}
+
+void OptionLayout::addOption(Option opt)
+{
+	optionList.push_back(opt) ;
+}
+
+void OptionLayout::addOption(string sh, string lg, string des)
+{
+	Option newOption(sh,lg,des) ;
+	optionList.push_back(newOption) ;
+}
+
+void OptionLayout::printOptions()
+{
+	cout << title << " :" << endl ;
+	list<Option>::iterator it ;
+	int lMax = getMaximumOptionLength() ;
+	for(it=optionList.begin() ; it!=optionList.end() ; it++)
+	{
+		string space = intToStrBlank(lMax - (*it).getLongOption().size() + 2) ;
+		cout << "  -" << (*it).getShortOption() << " ,  --" << (*it).getLongOption()
+				<< space << (*it).getDescription() << endl ;
+	}
+}
+
+int OptionLayout::getMaximumOptionLength()
+{
+	unsigned int lMax = 0 ;
+	list<Option>::iterator it ;
+	for(it=optionList.begin() ; it!=optionList.end() ; it++)
+	{
+		if((*it).getLongOption().size()>lMax) lMax = (*it).getLongOption().size() ;
+	}
+	return lMax ;
+}
+
+string OptionLayout::intToStrBlank(int i)
+{
+	string str = "" ;
+	for(int j=1 ; j<=i ; j++) str.push_back(' ') ;
+	return str ;
+}
---
+++ Menu/OptionLayout.h
@@ -0,0 +1,39 @@
+/*
+ *
+ * //My libraries for C++ usage
+ * Copyright (c) 2012 Rémi Eté
+ * All Right Reserved.
+ *
+ * Use and copying of these libraries and preparation of derivative works
+ * based upon these libraries are permitted. Any copy of these libraries
+ * must include this copyright notice.
+ *
+ * Written by : R. Eté
+ */
+
+#ifndef OPTIONLAYOUT_H
+#define OPTIONLAYOUT_H
+
+#include <iostream>
+#include <string>
+#include <list>
+#include "Option.h"
+
+class OptionLayout
+{
+private :
+	std::list<Option> optionList ;
+	std::string title ;
+	int getMaximumOptionLength() ;
+
+public :
+	OptionLayout(std::string t) ;
+	~OptionLayout() ;
+	void addOption(Option opt) ;
+	void addOption(std::string sh, std::string lg, std::string des) ;
+	void printOptions() ;
+	int listSize() ;
+	std::string intToStrBlank(int i) ;
+};
+
+#endif  // OPTIONLAYOUT_H
